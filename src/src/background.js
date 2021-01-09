9649d9687b19a5cef65188cbe7ae3f294344e5ba
rhaksw <rdevco.help@gmail.com>
2021-01-09 17:49:55 +0800

    only add quarantine cookie on reveddit.com request

--- src/src/background.js
+++ src/src/background.js
@@ -24,20 +24,22 @@ if (chrome && chrome.webRequest.OnBeforeSendHeadersOptions.hasOwnProperty('EXTRA
 }
 
 chrome.webRequest.onBeforeSendHeaders.addListener(function(details){
-    var newCookie = '_options={%22pref_quarantine_optin%22:true};'
-    var gotCookie = false
-    for (var n in details.requestHeaders) {
-        const headerName = details.requestHeaders[n].name.toLowerCase()
-        if (headerName === "cookie") {
-            details.requestHeaders[n].value = details.requestHeaders[n].value.replace(/ ?reddit_session[^;]*;/,'')
-            if (! details.requestHeaders[n].value.match(/pref_quarantine_optin/)) {
-                details.requestHeaders[n].value = details.requestHeaders[n].value + `; ${newCookie}`
+    if (details.initiator === 'https://www.reveddit.com') {
+        var newCookie = '_options={%22pref_quarantine_optin%22:true};'
+        var gotCookie = false
+        for (var n in details.requestHeaders) {
+            const headerName = details.requestHeaders[n].name.toLowerCase()
+            if (headerName === "cookie") {
+                details.requestHeaders[n].value = details.requestHeaders[n].value.replace(/ ?reddit_session[^;]*;/,'')
+                if (! details.requestHeaders[n].value.match(/pref_quarantine_optin/)) {
+                    details.requestHeaders[n].value = details.requestHeaders[n].value + `; ${newCookie}`
+                }
+                gotCookie = true
             }
-            gotCookie = true
         }
-    }
-    if (! gotCookie) {
-        details.requestHeaders.push({name:"Cookie",value:newCookie})
+        if (! gotCookie) {
+            details.requestHeaders.push({name:"Cookie",value:newCookie})
+        }
     }
     return {requestHeaders:details.requestHeaders}
 },{
