8e81127e22c915e0469a77089ab4c44f4dd4f4bb
rhaksw <rdevco.help@gmail.com>
2026-04-12 07:47:15 -0400

    Fix public path error

--- webpack.config.js
+++ webpack.config.js
@@ -138,6 +138,7 @@ export default {
         ...extensionPages
     },
     output: {
+        publicPath: '',
         path: distPath,
         filename: (pathData) => {
             const filename = '[name].js'
